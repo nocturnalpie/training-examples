ea98105f4bf59e0a4ceb43ca7db87fd2569e91e7
Nicole Livesey <nicole.livesey@microlise.com>
2025-07-10 17:01:13 +0100

    removed app styles property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,6 @@ import { HeaderComponent } from './header/header.component';
 		HeaderComponent,
 	],
 	templateUrl: './app.component.html',
-	styleUrl: './app.component.scss'
 })
 export class AppComponent {
 }
