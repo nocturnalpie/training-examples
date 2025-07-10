2f1020b94d8906abc10b6a43c9ac354b3c1d7e3d
Nicole Livesey <nicole.livesey@microlise.com>
2025-07-10 16:59:31 +0100

    moved routes into it's own file

---
+++ src/app/routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from "@angular/router";
+import { CartComponent } from "./cart/cart.component";
+import { ProductsComponent } from "./products/product-list/products.component";
+
+export const ROUTES: Routes = [
+	{
+		path: '',
+		component: ProductsComponent,
+		title: 'Products'
+	},
+	{
+		path: 'cart',
+		component: CartComponent,
+		title: 'Cart'
+	},
+];
