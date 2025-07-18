13028247a8de88e64baef1100e4621400f5826a1
Nicole Livesey <nicole.livesey@microlise.com>
2025-07-18 16:46:49 +0100

    Updated header to get the title from the route data

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,12 @@
-import { Component, effect, inject, viewChild } from '@angular/core';
+import { Component, computed, inject, viewChild } from '@angular/core';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatMenu, MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
+import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, Data, NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter, Observable, switchMap } from 'rxjs';
 import { CartSummaryComponent } from '../cart/cart-summary/cart-summary.component';
 import { CartService } from '../cart/service/cart.service.interface';
 
@@ -18,15 +20,32 @@ import { CartService } from '../cart/service/cart.service.interface';
 		MatIconModule,
 		MatBadgeModule,
 		MatMenuModule,
+		RouterModule,
 	],
 	templateUrl: './header.component.html',
 	styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+	
 	protected readonly cart = inject(CartService);
-	private menuTrigger = viewChild<MatMenuTrigger>(MatMenuTrigger);
-
+	protected pageTitle = computed(() => this.routeData()?.['title']);
+	
 	constructor() {
-		this.cart.itemAdded.subscribe(() => this.menuTrigger()?.openMenu());
+		this.cart.itemAdded
+		.pipe(takeUntilDestroyed())
+		.subscribe(() => this.menuTrigger()?.openMenu());
+	}
+	
+	private readonly router = inject(Router);
+	private readonly activatedRoute = inject(ActivatedRoute);
+	private menuTrigger = viewChild<MatMenuTrigger>(MatMenuTrigger);
+	
+	private routeData = toSignal(this.router.events.pipe(
+		filter(e => e instanceof NavigationEnd),
+		switchMap(() => this.getChildOrRouteData(this.activatedRoute)),
+	));
+
+	private getChildOrRouteData(route: ActivatedRoute): Observable<Data> {
+		return route.firstChild ? this.getChildOrRouteData(route.firstChild) : route.data;
 	}
 }
