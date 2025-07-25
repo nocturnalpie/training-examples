1c022e6e0dc0dbb9a6f5eef24e6e918928b09af7
Nicole Livesey <nicole.livesey@microlise.com>
2025-07-25 18:21:11 +0100

    formatting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { TitleStrategy, provideRouter, withComponentInputBinding } from '@angular/router';
 import 'zone.js';
 import { AppComponent } from './app.component';
+import { ROUTES } from './routes';
+import { TemplatePageTitleStrategy } from './title.strategy';
 
 /* Choose an implementation here */
 // import { provideNgRxStore } from './app/cart/service/ngRx-implementation/cart-ngrx.service';
 import { provideSignalsStore } from './cart/service/signals-implementation/cart-signals.service';
-import { ROUTES } from './routes';
-import { TemplatePageTitleStrategy } from './title.strategy';
 
 export const STORE_NAME = 'Toy Shop';
 
