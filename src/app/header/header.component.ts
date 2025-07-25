696dedd4af7f2fc870417f2bd9a810dfa8b7277f
Nicole Livesey <nicole.livesey@microlise.com>
2025-07-25 18:20:52 +0100

    Added button to toggle between light and dark mode

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,37 +1,48 @@
 import { Component, inject, viewChild } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatBadge } from '@angular/material/badge';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatTooltip } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
 import { CartComponent } from '../cart/cart.component';
 import { CartService } from '../cart/service/cart.service.interface';
+import { ThemeModeService } from '../theme-mode/theme-mode.service';
 
 @Component({
 	selector: 'app-header',
 	imports: [
 		CartComponent,
 		RouterModule,
-		MatToolbarModule,
-		MatButtonModule,
-		MatIconModule,
-		MatBadgeModule,
-		MatMenuModule,
+		MatToolbar,
+		MatButton,
+		MatIconButton,
+		MatIcon,
+		MatBadge,
+		MatMenu,
+		MatMenuTrigger,
+		MatTooltip,
 	],
 	templateUrl: './header.component.html',
 	styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-	
 	protected readonly cart = inject(CartService);
-	
+
+	private readonly themeModeService = inject(ThemeModeService);
+	protected themeDetails = this.themeModeService.themeDetails;
+
 	private menuTrigger = viewChild<MatMenuTrigger>(MatMenuTrigger);
 
 	constructor() {
 		this.cart.itemAdded
 			.pipe(takeUntilDestroyed())
 			.subscribe(() => this.menuTrigger()?.openMenu());
+	}
+
+	toggleThemeMode(): void {
+		this.themeModeService.toggleThemeMode();
 	}
 }
