0c8f81645a5a9c47586ca843666bbdf7f8d8b8cf
Nicole Livesey <nicole.livesey@microlise.com>
2025-09-04 15:40:11 +0100

    sorted out list items

--- src/app/cart/cart-page/cart-page.component.ts
+++ src/app/cart/cart-page/cart-page.component.ts
@@ -1,16 +1,14 @@
 import { Component, inject } from '@angular/core';
-import { MatButtonModule, MatIconButton } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { MatList, MatListItem } from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
 import { CartService } from '../service/cart.service.interface';
 
 @Component({
 	selector: 'app-cart-page',
 	imports: [
-		MatList,
-		MatListItem,
+		MatListModule,
 		MatIcon,
-		MatIconButton,
 		MatButtonModule,
 	],
 	templateUrl: './cart-page.component.html',
